70e04dfd12f00531488ae167121203151ca544a3
Skander Mejdi <skander.mejdi@epitech.eu>
2017-04-28 01:22:46 -0700

    beginning integration of theme

--- src/App.js
+++ src/App.js
@@ -1,18 +1,66 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import HeaderContent from './HeaderContent';
+import About from './About';
+import './css/animate.css';
+import './css/bootstrap.min.css';
+import './css/font-awesome.min.css';
+import './css/main.css';
 
 class App extends Component {
+
+  constructor(props, event) {
+    super(props)
+    this.state = {'nav': ''};
+    this.handleScroll = this.handleScroll.bind(this);
+  }
+
+  componentDidMount() {
+      window.addEventListener('scroll', this.handleScroll);
+  }
+
+  componentWillUnmount() {
+      window.removeEventListener('scroll', this.handleScroll);
+  }
+
+  handleScroll(event) {
+    if (event.pageY > 50) {
+      this.setState({'nav': 'top-nav-collapse'});
+    } else {
+      this.setState({'nav': ''});
+    }
+  }
+
   render() {
+
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+      <div className="header">
+        <div className="bg-color">
+          <header id="main-header">
+            <nav className={'navbar navbar-default navbar-fixed-top ' + this.state.nav}>
+              <div className="container">
+                <div className="navbar-header">
+                  <button type="button" className="navbar-toggle" data-toggle="collapse" data-target="#lauraMenu">
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                    <span className="icon-bar"></span>
+                  </button>
+                  <a className="navbar-brand" href="#">François</a>
+                </div>
+                <div className="collapse navbar-collapse" id="lauraMenu">
+                  <ul className="nav navbar-nav navbar-right navbar-border">
+                    <li className="active"><a href="#main-header">Home</a></li>
+                    <li><a href="#about">About</a></li>
+                    <li><a href="#portfolio">Portfolio</a></li>
+                    <li><a href="#testimonial">Testimonial</a></li>
+                    <li><a href="#contact">Contact Us</a></li>
+                  </ul>
+                </div>
+              </div>
+            </nav>
+          </header>
+          <HeaderContent />
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <About />
       </div>
     );
   }
---
+++ src/HeaderContent.js
@@ -0,0 +1,25 @@
+import React, { Component } from 'react';
+
+class HeaderContent extends Component {
+  render() {
+    return (
+      <div className="wrapper">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12 wow fadeIn delay-05s">
+              <div className="banner-text">
+                <h2>Hi, I am <span>François</span></h2>
+                <p>The perfect advisor for your <br /> next tv session</p>
+              </div>
+              <div className="overlay-detail text-center">
+                <a href="#about"><i className="fa fa-angle-down"></i></a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default HeaderContent;
