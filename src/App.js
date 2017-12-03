2e769ec44f62d5b1832cb7bfc8f4a869de25b334
Skander Mejdi <mejdi.skander@gmail.com>
2017-12-03 18:04:45 +0100

    reset

--- src/App.js
+++ src/App.js
@@ -1,27 +1,21 @@
 import React, { Component } from 'react';
-import Header from './Header';
-import Landing from './Landing';
-import About from './About';
-import './css/animate.css';
-import './css/bootstrap.min.css';
-import './css/font-awesome.min.css';
-import './css/main.css';
+import logo from './logo.svg';
+import './App.css';
 
 class App extends Component {
-
   render() {
-
     return (
-      <div className="header">
-        <div className="bg-color">
-          <Header />
-          <Landing />
-        </div>
-        <About />
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
       </div>
     );
   }
-
 }
 
 export default App;
